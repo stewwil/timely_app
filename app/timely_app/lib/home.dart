50ff98d92f4f552f13ef0d3caed633bf1d2d2785
William Stewart <cheerswilly@gmail.com>
2020-05-18 20:37:47 -0400

    added home.dart

---
+++ app/timely_app/lib/home.dart
@@ -0,0 +1,9 @@
+//some basic user info variable declaration/storage 
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  TextEditingController _emailController = new TextEditingController();
+  TextEditingController _passwordController = new TextEditingController();
+  TextEditingController _nameController = new TextEditingController();
+  String _email;
+  String _password;
+  String _displayName;
+  bool _obsecure = false;
